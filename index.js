19738f42005d6d20be4b666b5d5b56dca4d4072f
kevinhui98 <kevin.hui@envirostudies.org>
2017-01-05 23:16:29 +0000

    making the input and submit responsive

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
+    $("#submitButton").click(function(){
+    var changeSearch = $("#submitText").val();
+
    $.getJSON(
      
-      "https://api.giphy.com/v1/gifs/search?q=HIMYM barney stinston&api_key=dc6zaTOxFJmzC",
+      "https://api.giphy.com/v1/gifs/search?q=" + changeSearch + "&api_key=dc6zaTOxFJmzC",
      
      
      
       function(response) {
         console.log(response);
+        $("#gif").append("<img src=" + response.data[0].images.fixed_width_downsampled.url + ">");
         $("#gif").append("<img src=" + response.data[1].images.fixed_width_downsampled.url + ">");
-        $("#gif").append("<img src=" + response.data[8].images.fixed_width_downsampled.url + ">");
-        $("#gif").append("<img src=" + response.data[5].images.fixed_width_downsampled.url + ">");
+        $("#gif").append("<img src=" + response.data[2].images.fixed_width_downsampled.url + ">");
         $("#gif").append("<img src=" + response.data[3].images.fixed_width_downsampled.url + ">");
         $("#gif").append("<img src=" + response.data[4].images.fixed_width_downsampled.url + ">");
-        $("#gif").append("<img src=" + response.data[7].images.fixed_width_downsampled.url + ">");
+        $("#gif").append("<img src=" + response.data[5].images.fixed_width_downsampled.url + ">");
       });
+    });
   });

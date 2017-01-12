eb7564d0e151bda3e96841a4b8a1c105bd814fed
kevinhui98 <kevin.hui@envirostudies.org>
2017-01-12 22:28:34 +0000

    second commit

--- index.js
+++ index.js
@@ -2,20 +2,15 @@ $(document).ready(function() {
     $("#submitButton").click(function(){
     var changeSearch = $("#submitText").val();
 
+$.getJSON('http://ajar-target.gomix.me/status')
    $.getJSON(
      
       "https://api.giphy.com/v1/gifs/search?q=" + changeSearch + "&api_key=dc6zaTOxFJmzC",
      
-     
-     
       function(response) {
         console.log(response);
         $("#gif").append("<img src=" + response.data[0].images.fixed_width_downsampled.url + ">");
-        $("#gif").append("<img src=" + response.data[1].images.fixed_width_downsampled.url + ">");
-        $("#gif").append("<img src=" + response.data[2].images.fixed_width_downsampled.url + ">");
-        $("#gif").append("<img src=" + response.data[3].images.fixed_width_downsampled.url + ">");
-        $("#gif").append("<img src=" + response.data[4].images.fixed_width_downsampled.url + ">");
-        $("#gif").append("<img src=" + response.data[5].images.fixed_width_downsampled.url + ">");
       });
+     
     });
   });

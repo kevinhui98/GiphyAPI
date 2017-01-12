c0809c9ba0130df47d7769ddce7fbb656bbfba16
kevinhui98 <kevin.hui@envirostudies.org>
2017-01-12 22:58:54 +0000

    adding the mta stops

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+/*$(document).ready(function() {
+    $("#mtaButton").click(function(){
+    var status = $("#mtaText").val();*/
 $(document).ready(function() {
     $("#submitButton").click(function(){
     var changeSearch = $("#submitText").val();
-
-$.getJSON('http://ajar-target.gomix.me/status')
+}
+$.getJSON(
+    'http://ajar-target.gomix.me/stops', function(response) {
+});
    $.getJSON(
      
       "https://api.giphy.com/v1/gifs/search?q=" + changeSearch + "&api_key=dc6zaTOxFJmzC",
@@ -10,7 +15,9 @@ $.getJSON('http://ajar-target.gomix.me/status')
       function(response) {
         console.log(response);
         $("#gif").append("<img src=" + response.data[0].images.fixed_width_downsampled.url + ">");
-      });
-     
-    });
+     /*});
+    });*/
+   });
   });
+ //});
+     
